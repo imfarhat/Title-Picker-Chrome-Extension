f5d2d69a70d59b165923201f4afdb67a959914a5
imfarhat <ifarhat.dev@gmail.com>
2024-05-05 20:51:08 +0530

    feat: add copy button

--- popup.js
+++ popup.js
@@ -1,11 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   let getTitleButton = document.getElementById("getTitleButton");
+  let copyButton = document.getElementById("copyButton");
+
   getTitleButton.addEventListener("click", function () {
     document.getElementById("tabTitle").textContent = "Loading...";
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       let tab = tabs[0];
       document.getElementById("tabTitle").textContent = tab.title;
-      document.getElementById("copyButton").classList.remove("hidden");
+      copyButton.classList.remove("hidden");
     });
   });
+
+  copyButton.addEventListener("click", function () {
+    const tabTitle = document.getElementById("tabTitle").textContent;
+    copyTextToClipboard(tabTitle);
+    copyButton.textContent = "Title Copied";
+    copyButton.disabled = true;
+    setTimeout(() => {
+      copyButton.textContent = "Copy Title";
+      copyButton.disabled = false;
+    }, 1500);
+  });
 });
+
+function copyTextToClipboard(text) {
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
