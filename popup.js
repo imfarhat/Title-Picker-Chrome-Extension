9dcd2813f586295a4507fbd4944b1fd1944941dd
imfarhat <ifarhat.dev@gmail.com>
2024-05-05 20:23:42 +0530

    feat: add loading state to div content

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   let getTitleButton = document.getElementById("getTitleButton");
   getTitleButton.addEventListener("click", function () {
+    document.getElementById("tabTitle").textContent = "Loading...";
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       let tab = tabs[0];
       document.getElementById("tabTitle").textContent = tab.title;
