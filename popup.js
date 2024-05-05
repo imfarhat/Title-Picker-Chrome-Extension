0dcd3cae4195338b7b2ab7e6f98232a00d08efc1
imfarhat <ifarhat.dev@gmail.com>
2024-05-05 20:40:42 +0530

    feat: show hide copy button

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       let tab = tabs[0];
       document.getElementById("tabTitle").textContent = tab.title;
+      document.getElementById("copyButton").classList.remove("hidden");
     });
   });
 });
